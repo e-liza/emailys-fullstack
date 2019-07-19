1240fbf6e701b3cc57683cd8d595c101ac4fc308
e-liza <eliza.lks@gmail.com>
2019-07-19 07:11:05 +0200

    Initializing app component with browser router and redux, fetching user data on mount

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-function App() {
-  return <div className="App" />;
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div />
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect(
+  null,
+  actions
+)(App);

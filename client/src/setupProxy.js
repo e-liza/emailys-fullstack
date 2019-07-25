d912787938db6e528671f223400cd88cce722a11
e-liza <eliza.lks@gmail.com>
2019-07-25 23:11:11 +0200

    Adding proxy rules

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -9,4 +9,7 @@ module.exports = function(app) {
   app.use(
     proxy('/api/*', { target: 'http://localhost:5000/' })
   );
+  app.use(
+    proxy('/api/*/*', { target: 'http://localhost:5000/' })
+  );
 };

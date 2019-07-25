bb90f77e5d32dd3c35600baf7818761f906e58a6
e-liza <eliza.lks@gmail.com>
2019-07-25 13:39:00 +0200

    Adding route to post data coming from form in frontend, creating a new survey object with the data

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -4,3 +4,22 @@ const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
 const Survey = mongoose.model('surveys');
+
+
+module.exports = app => {
+
+    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
+        const { title, subject, body, recipients } = req.body;
+    
+        const survey = new Survey({
+          title,
+          subject,
+          body,
+          recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+          _user: req.user.id,
+          dateSent: Date.now()
+        });
+
+    });
+
+}

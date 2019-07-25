3f76e64adfaae3696f55c6026b5524615e18d55e
e-liza <eliza.lks@gmail.com>
2019-07-25 21:20:42 +0200

    Sendgrid functionality and mongo logic for the route /api/surveys

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,9 +1,14 @@
 const mongoose = require('mongoose');
+const sgMail = require('@sendgrid/mail');
+const keys = require('../config/keys');
 
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const mailer = require('../services/mailer');
 
 const Survey = mongoose.model('surveys');
+
+sgMail.setApiKey(keys.sendGridKey);
 
 module.exports = app => {
   app.post(
@@ -23,6 +28,18 @@ module.exports = app => {
         _user: req.user.id,
         dateSent: Date.now()
       });
+
+      const msg = mailer(survey);
+
+      try {
+        await sgMail.send(msg);
+        await survey.save();
+        req.user.credits -= 1;
+        const user = await req.user.save();
+        res.send(user);
+      } catch (err) {
+        res.status(422).send(err);
+      }
     }
   );
 };

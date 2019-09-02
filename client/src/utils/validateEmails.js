1586bb8d438b667ca32b6358b816dbde0e956887
e-liza <eliza.lks@gmail.com>
2019-09-02 19:30:56 +0200

    Remove trailing spaces

--- client/src/utils/validateEmails.js
+++ client/src/utils/validateEmails.js
@@ -1,6 +1,8 @@
 const re = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
 export default emails => {
-  const invalidEmails = emails.split(',');
+  const invalidEmails = emails
+    .split(',')
+    .map(email => email.trim());
   return;
 };

29380e60947f1754fccd908ff25e5996894c3db6
e-liza <eliza.lks@gmail.com>
2019-09-03 11:15:29 +0200

    Initial structure of SurveyFormReview with reviewFields method that outputs the form fields and mapStateToProps, that connects the values from SurveyForm to SurveyFormReview in order to output them for the review

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -5,3 +5,11 @@ import { connect } from 'react-redux';
 import formFields from './formFields';
 import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
+
+const SurveyFormReview = () => {
+  const reviewFields = () => {};
+
+  return;
+};
+
+function mapStateToProps(state) {}

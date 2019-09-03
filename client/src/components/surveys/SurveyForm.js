a54978c8082b938f1ed28fbba73b148738bd1ae1
e-liza <eliza.lks@gmail.com>
2019-09-03 10:54:39 +0200

    Adding an onSubmit event to a form, using a handleSubmit prop from redux-form, while passing to it a custom prop caled onSurveySubmit (defined in SurveyNew), takes the user to the next page of the wizard form (SurveyFormReview)

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -24,7 +24,11 @@ class SurveyForm extends Component {
   render() {
     return (
       <div>
-        <form>
+        <form
+          onSubmit={this.props.handleSubmit(
+            this.props.onSurveySubmit
+          )}
+        >
           {this.renderFields()}
           <Link
             to="/surveys"

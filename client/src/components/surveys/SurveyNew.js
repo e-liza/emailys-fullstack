e4b85a948137f13c39c8090cf3bb784146dc39a8
e-liza <eliza.lks@gmail.com>
2019-09-03 10:31:13 +0200

    redux form decorator for the form wrapper

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -4,11 +4,21 @@ import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
-  state = { showFormReview: false };
-
-  renderContent() {}
-
-  render() {
-    return <div>{this.renderContent()}</div>;
+    state = { showFormReview: false };
+  
+    renderContent() {
+    
+    }
+  
+    render() {
+      return (
+        <div>
+          {this.renderContent()}
+        </div>
+      );
+    }
   }
-}
+  
+  export default reduxForm({
+    form: 'surveyForm'
+  })(SurveyNew);

cbaac3966d88eae23a20f6620394619faa6a09fb
e-liza <eliza.lks@gmail.com>
2019-09-03 11:11:48 +0200

    Imports for SurveyFormReview, withRouter is needed in order to pass a history object to the action creator, called submitSurvey that should be used in order to redirect the user to the dahsboard on form submit, without it SurveyFormReview and action creator are not aware of the react router

---
+++ client/src/components/surveys/SurveyFormReview.js
@@ -0,0 +1,7 @@
+// SurveyFormReview shows users their form inputs for review
+import _ from 'lodash';
+import React from 'react';
+import { connect } from 'react-redux';
+import formFields from './formFields';
+import { withRouter } from 'react-router-dom';
+import * as actions from '../../actions';

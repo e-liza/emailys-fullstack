7ae62445fb868af725c155bdba33584d6f83979e
e-liza <eliza.lks@gmail.com>
2019-09-03 11:32:12 +0200

    Adding a button to create a new survey(SurveyNew-form wizard) to dahsboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,7 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
-  return <div />;
+  return (
+    <div>
+      <div className="fixed-action-btn">
+        <Link
+          to="/surveys/new"
+          className="btn-floating btn-large red"
+        >
+          <i className="material-icons">add</i>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;

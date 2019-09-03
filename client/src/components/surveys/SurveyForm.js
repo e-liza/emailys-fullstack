58339d1b747ab2192dfff2cab38e5b994224dd67
e-liza <eliza.lks@gmail.com>
2019-09-03 10:37:39 +0200

    Imports for SurveyForm (first page of the wizard form

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,7 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import SurveyField from './SurveyField';
+import validateEmails from '../../utils/validateEmails';
+import formFields from './formFields';

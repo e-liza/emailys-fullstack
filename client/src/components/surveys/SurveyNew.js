fe8270ec892c6bf855c2a8a558977f3d3b9a3bb7
e-liza <eliza.lks@gmail.com>
2019-09-03 10:30:28 +0200

    Adding a state and a method, that decide which page of the wizard form to render

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -2,3 +2,13 @@ import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
 import SurveyFormReview from './SurveyFormReview';
+
+class SurveyNew extends Component {
+  state = { showFormReview: false };
+
+  renderContent() {}
+
+  render() {
+    return <div>{this.renderContent()}</div>;
+  }
+}

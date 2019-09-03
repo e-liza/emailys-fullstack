9481cca1e48c43ee7d6bd448836bffc10607cb86
e-liza <eliza.lks@gmail.com>
2019-09-03 10:49:41 +0200

    Adding a next button, that is a submit button, that uses a submit method that will be added to form, when user clicks the button,the app shows the SurveyFormReview

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -22,16 +22,17 @@ class SurveyForm extends Component {
   }
 
   render() {
-    return (
-      <div>
+    return(
+        <div>
         <form>
           {this.renderFields()}
-          <Link
-            to="/surveys"
-            className="red btn-flat white-text"
-          >
+          <Link to="/surveys" className="red btn-flat white-text">
             Cancel
           </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next
+            <i className="material-icons right">done</i>
+          </button>
         </form>
       </div>
     );

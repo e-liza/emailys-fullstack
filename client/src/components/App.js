28fc25a5599774c59443dd4c7fd857b23dc02cbd
e-liza <eliza.lks@gmail.com>
2019-09-03 12:01:56 +0200

    Adding a route for SurveyNew

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,6 +6,7 @@ import * as actions from '../actions';
 import Header from './Header';
 import Landing from './Landing';
 import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
 
 class App extends Component {
   componentDidMount() {
@@ -23,6 +24,10 @@ class App extends Component {
               exact
               path="/surveys"
               component={Dashboard}
+            />
+            <Route
+              path="/surveys/new"
+              component={SurveyNew}
             />
           </div>
         </BrowserRouter>

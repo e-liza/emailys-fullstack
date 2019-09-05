dad1777b8417a921723e40fe6f27e6a70a7c344d
e-liza <eliza.lks@gmail.com>
2019-09-05 17:57:23 +0200

    Add a property called lastResponded that shows when is the last time the survey was taken

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -44,7 +44,8 @@ module.exports = app => {
           },
           {
             $inc: { [choice]: 1 },
-            $set: { 'recipients.$.responded': true }
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
           }
         );
       })

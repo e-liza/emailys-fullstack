4e6d1b096aec384957b5b69640d0aa5a0c2f08c4
e-liza <eliza.lks@gmail.com>
2019-09-05 17:33:25 +0200

    test the path from the url from sendgrid response against the path-parser rule

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -22,7 +22,9 @@ module.exports = app => {
   app.post('/api/surveys/webhooks', (req, res) => {
     const p = new Path('/api/surveys/:surveyId/:choice');
     _.chain(req.body)
-      .map(() => {})
+      .map(({ email, url }) => {
+        const match = p.test(new URL(url).pathname);
+      })
       .value();
     res.send({});
   });
